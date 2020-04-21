660aea94b6019ef50d72ded2bc75659847d58dc9
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-04-21 22:13:48 +0200

    Update Host_Setup.sh

--- Host_Setup.sh
+++ Host_Setup.sh
@@ -76,10 +76,9 @@ sudo apt update
 sudo apt install ansible -y
 
 #Docker CE Setup - Latest
-sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt update
+sudo apt -y update
 apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo systemctl status docker
+sudo apt install -y docker-ce
