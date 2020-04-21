93ef6dffd8128567986103757f626f9a143e422b
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-04-21 15:51:04 +0200

    + VSCode

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,10 @@ if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
 fi
+
+#Curl
+sudo apt install -y curl
+
+#Visual Studio Code
+curl https://az764295.vo.msecnd.net/stable/ff915844119ce9485abfe8aa9076ec76b5300ddd/code_1.44.2-1587059832_amd64.deb --output /home/$USER/Downloads/code.deb
+sudo dpkg -i /home/$USER/Downloads/code.deb
