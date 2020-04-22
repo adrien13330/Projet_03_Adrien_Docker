066e129ebca0f4d0f3e01943038914d897bc0437
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-04-22 15:37:21 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,9 @@
-apt-get update
-apt-get install -y apache2
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
-fi
-
-#Curl
-sudo apt install -y curl
-
 #Visual Studio Code
 curl https://az764295.vo.msecnd.net/stable/ff915844119ce9485abfe8aa9076ec76b5300ddd/code_1.44.2-1587059832_amd64.deb --output /home/$USER/Downloads/code.deb
 sudo dpkg -i /home/$USER/Downloads/code.deb
+
+#Ansible Setup - Latest
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+sudo apt update
+sudo apt install ansible -y
