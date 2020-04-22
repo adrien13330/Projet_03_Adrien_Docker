71ebf42405266543bc1554f58747c0b00c89b007
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-04-22 15:42:05 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,13 @@ echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | sudo te
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt update
 sudo apt install ansible -y
+
+#Docker Setup - Latest
+sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt -y update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+pkill -KILL -u $USER
