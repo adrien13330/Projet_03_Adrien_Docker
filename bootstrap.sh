39d05d2b1890ab4164dbeb479d2eb41e482c1115
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-04-23 14:03:55 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,17 @@
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt install -y xfce4
-sudo apt -y install software-properties-common apt-transport-https curl
+sudo apt install -y software-properties-common apt-transport-https curl
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt -y update
+sudo apt update -y
 sudo apt install -y code
 
 #Vagrant Bootstrap Setup: Ansible - Latest
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt -y update
-sudo apt -y install ansible
+sudo apt update -y
+sudo apt install ansible -y
 
 #Vagrant Bootstrap Setup: Docker - Latest
 sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
