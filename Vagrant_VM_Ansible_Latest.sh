8c6441507631eb1e6a4a806c556f0d889d23b670
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-04-23 11:48:41 +0200

    Update Vagrant_VM_Ansible_Latest.sh

--- Vagrant_VM_Ansible_Latest.sh
+++ Vagrant_VM_Ansible_Latest.sh
@@ -1,4 +1,6 @@
-deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main
+#Vagrant Bootstrap Setup : Ansible - Latest
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt -y update
 sudo apt -y install ansible
+
