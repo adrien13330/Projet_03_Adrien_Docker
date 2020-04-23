9d24a8f78f6bc83ed0c0f19e5cd68788d432932f
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-04-23 13:08:41 +0200

    Delete Vagrant_VM_Setup_Ansible_Latest.sh

--- Vagrant_VM_Setup_Ansible_Latest.sh
+++
@@ -1,6 +0,0 @@
-#Vagrant Bootstrap Setup : Ansible - Latest
-echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt -y update
-sudo apt -y install ansible
-
