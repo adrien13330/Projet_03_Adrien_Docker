1cce09140ad0ada69358ac0ee5e1321d1337136d
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-04-23 13:08:55 +0200

    Delete Vagrant_VM_Setup_VSCode.sh

--- Vagrant_VM_Setup_VSCode.sh
+++
@@ -1,8 +0,0 @@
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt install -y xfce4
-sudo apt -y install software-properties-common apt-transport-https curl
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt -y update
-sudo apt install -y code
