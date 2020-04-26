64c8dc94b52865c1087c3d405f0aa1ccc07a2aed
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-04-26 22:33:06 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,10 +15,10 @@ sudo apt update -y
 sudo apt install ansible -y
 
 #Vagrant Bootstrap Setup: Docker - Latest
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt update -y
+sudo apt install -y docker docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker vagrant
