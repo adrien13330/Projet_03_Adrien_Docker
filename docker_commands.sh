bfcc6acb9ab52ae3f8e190f2dd73b475b5fb6550
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-04-29 10:11:18 +0200

    Update docker_commands.sh

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,6 +1,6 @@
 docker pull debian
 docker build -t test .
-docker container run -d -p 9090:22 test
+docker container run -d -p 9090:22 -p 80:80 test
 
 ssh root@localhost -p 9090
 #ssh root@172.17.0.1 -p 9090
