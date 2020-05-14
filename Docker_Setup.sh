93f38baf97d7c98b4c7f6bc6aac97fc2a39a3ce0
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-05-14 21:16:00 +0200

    Update Docker_Setup.sh

--- Docker_Setup.sh
+++ Docker_Setup.sh
@@ -1,5 +1,8 @@
 docker pull debian
 docker build -t test .
-docker run -d -p 2222:22 -p 80:80 test
+#docker run -d -p 2222:22 -p 80:80 test
+
+docker run -dit --restart unless-stopped -p 80:80 --name test -d nginx
+
 
 # ssh root@localhost -p 2222
